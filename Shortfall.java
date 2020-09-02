586b7b7df6dd545f98dc081bde75f70b743874d1
LiAn Song <songlian7@gmail.com>
2020-09-02 11:14:54 -0600

    completed chapter 8 exercise 1

---
+++ Shortfall.java
@@ -0,0 +1,7 @@
+public class Shortfall { 
+    public static void main(String[] args) {
+        // short value = 150090000; short can only take up to 16 bits
+        int value = 1500901890;
+        System.out.println("A short: "  + value);
+    }
+}
