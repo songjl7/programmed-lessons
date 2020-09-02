b8d02371d177c49f19f8c3bf59f48294da91cb44
LiAn Song <songlian7@gmail.com>
2020-09-02 11:23:57 -0600

    completed chapter 8 exercise 2

---
+++ DoubleJeopardy.java
@@ -0,0 +1,9 @@
+public class DoubleJeopardy
+{
+  public static void main ( String[] args )
+  {
+    // double returns a float that uses 64 bits
+    double value = 32;
+    System.out.println("A double: " +  value);
+  }
+}
