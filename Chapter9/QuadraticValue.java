a97b366f8e198c42b9291f8fe29a2b7c812d95a9
LiAn Song <songlian7@gmail.com>
2020-09-03 18:49:33 -0600

    complete chapter 9 exercise 3

--- Chapter9/QuadraticValue.java
+++ Chapter9/QuadraticValue.java
@@ -1,7 +1,15 @@
 public class QuadraticValue{
     public static void main(String[] args){
         double x;
-        x = 2;
-        System.out.println("At x = " + x + ", the value is " + (3*x*x - 8*x + 4));
+        double value;
+        x = 0.0;
+        value = 3*x*x - 8*x + 4;
+        System.out.println("At x = " + x + ", the value is " + value);
+        x = 2.0;
+        value = 3*x*x - 8*x + 4;
+        System.out.println("At x = " + x + ", the value is " + value);
+        x = 4.0;
+        value = 3*x*x - 8*x + 4;
+        System.out.println("At x = " + x + ", the value is " + value);
     }
 }
