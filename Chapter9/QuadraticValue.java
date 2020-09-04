80b9e0381e5baacd1fa90366a1a6ed344ad7fe4e
LiAn Song <songlian7@gmail.com>
2020-09-03 18:41:43 -0600

    completed chapter 9 exercise 2

---
+++ Chapter9/QuadraticValue.java
@@ -0,0 +1,7 @@
+public class QuadraticValue{
+    public static void main(String[] args){
+        double x;
+        x = 2;
+        System.out.println("At x = " + x + ", the value is " + (3*x*x - 8*x + 4));
+    }
+}
