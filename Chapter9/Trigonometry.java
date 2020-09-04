68f847ab341f0946c537ffd51faa6d8e8b52bd4e
LiAn Song <songlian7@gmail.com>
2020-09-03 19:05:53 -0600

    completed chapter 10 exercise 3

--- Chapter9/Trigonometry.java
+++ Chapter9/Trigonometry.java
@@ -1,7 +1,9 @@
 public class Trigonometry{
     public static void main(String[] args){
-        double sinx = Math.sin(0.5236);
-        double cosx = Math.cos(0.5236);
+        double degrees = 30;
+        double radians = degrees * Math.PI/180;
+        double sinx = Math.sin(radians);
+        double cosx = Math.cos(radians);
         double total = sinx*sinx + cosx*cosx;
         System.out.println("sine: " + sinx + " cosine: " + cosx + " sum of squares: " + total);
     }
