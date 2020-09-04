c0d74d5d1608dd67dbfd217d0fbc41d54d227117
LiAn Song <songlian7@gmail.com>
2020-09-03 18:24:18 -0600

    completed chapter 8 exercise 4

---
+++ CharAssassination.java
@@ -0,0 +1,10 @@
+public class CharAssassination
+{
+  public static void main ( String[] args )
+  {
+    // characters are 16 bits of main memory
+    // Can only use '' not double quotes, those are for Strings
+    char ch = 'A' ;
+    System.out.println("A char: " +  ch );
+  }
+}
