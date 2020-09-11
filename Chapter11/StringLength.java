4f58abb7fee9d3236699ae37a09e5a18d4073ad4
LiAn Song <songlian7@gmail.com>
2020-09-11 14:44:14 -0600

    completed chapter 11 exercise 2

---
+++ Chapter11/StringLength.java
@@ -0,0 +1,15 @@
+public class StringLength
+{
+  public static void main ( String[] args )
+  {
+    String str;
+    int    len;
+
+    str = new String( " spacey     " );
+
+    len = str.length();
+
+    System.out.println("The length is: " + len );
+
+  }
+}
