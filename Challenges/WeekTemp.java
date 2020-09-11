00118cd7a50d364e6c91bb54744e78c233fc0503
LiAn Song <songlian7@gmail.com>
2020-09-11 11:29:51 -0600

    simplified code by making scanned values into ints rather than parseInting strings

--- Challenges/WeekTemp.java
+++ Challenges/WeekTemp.java
@@ -5,21 +5,21 @@ public class WeekTemp {
         System.out.println("The program will ask you to enter temperatures. Please round to the nearest degree Fahrenheit.");
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Monday temperature");
-        String tempm = scan.next();
+        int tempm = scan.nextInt();
         System.out.println("Enter Tuesday temperature");
-        String tempt = scan.next();
+        int tempt = scan.nextInt();
         System.out.println("Enter Wednesday temperature");
-        String tempw = scan.next();
+        int tempw = scan.nextInt();
         System.out.println("Enter Thursday temperature");
-        String tempth = scan.next();
+        int tempth = scan.nextInt();
         System.out.println("Enter Friday temperature");
-        String tempf = scan.next();
+        int tempf = scan.nextInt();
         System.out.println("Enter Saturday temperature");
-        String tempsa = scan.next();
+        int tempsa = scan.nextInt();
         System.out.println("Enter Sunday temperature");
-        String tempsu = scan.next();
+        int tempsu = scan.nextInt();
         System.out.println("Monday:\t\t" + tempm + "\nTuesday:\t" + tempt + "\nWednesday:\t" + tempw + "\nThursday:\t" + tempth + "\nFriday:\t\t" + tempf + "\nSaturday:\t" + tempsa + "\nSunday:\t\t" + tempsu);
-        double avg = (Integer.parseInt(tempm) + Integer.parseInt(tempt) + Integer.parseInt(tempw) + Integer.parseInt(tempth) + Integer.parseInt(tempf) + Integer.parseInt(tempsa) + Integer.parseInt(tempsu)) / 7.0;
+        double avg = (tempm + tempt + tempw + tempth + tempf + tempsa + tempsu) / 7.0;
         System.out.println("Average temperature of the week: " + avg);
     }
 }
