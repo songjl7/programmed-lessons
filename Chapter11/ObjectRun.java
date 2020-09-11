08a299d8a6fabe2c3ed1c022bad76d5f411fd51a
LiAn Song <songlian7@gmail.com>
2020-09-11 14:40:37 -0600

    completed chapter 11 exercise 1

---
+++ Chapter11/ObjectRun.java
@@ -0,0 +1,9 @@
+public class ObjectRun {
+    public static void main(String[] args){
+        String str = new String("Hello World");
+        System.out.println(str);
+        String secondVariable;
+        System.out.println(secondVariable);
+        // Objects and variables are different!
+    }
+}
