57e104664b837fe82d65de90d61190bba1c2a802
LiAn Song <songlian7@gmail.com>
2020-10-07 10:54:35 -0600

    complete chapter 24 exercise 3

---
+++ Chapter24/OddSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OddSum{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter n");
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < n; i ++){
+            if (n % i  == 0 ){
+                sum = sum + i;
+            }
+        }
+        if (n == sum){
+            System.out.println("n is a perfect number.");
+        }
+
+        System.out.println("Sum of odds: " + sum);
+    }
+}
