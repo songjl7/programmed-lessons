724ed5048e2bc62fe272de7077b181807a4f7627
LiAn Song <songlian7@gmail.com>
2020-11-13 11:00:06 -0700

    first commit

---
+++ Challenges/LunchAccount.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import java.text.DecimalFormat;
+
+public class LunchAccount {
+    Random rand = new Random();
+    DecimalFormat df = new DecimalFormat("#,##0.00");
+
+    private int idNum;
+    private double balance;
+    private double total = 0.00;
+    
+    private static int lunchesBought = 0;
+    private static int lunchAccountCount = 98;
+
+    public LunchAccount(){
+        lunchAccountCount ++;
+        this.idNum = rand.nextInt(10000);
+        this.balance = 0.0;
+    }
+
+    public LunchAccount(double balance){
+        lunchAccountCount ++;
+        this.idNum = rand.nextInt(10000);
+        if (lunchAccountCount <= 100){
+            this.balance += 20.00;
+        } else {
+            this.balance = balance;
+        }
+    }
+
+    public void addMoney(double amount){
+        if (lunchAccountCount <= 100 && amount > 0.0){
+            this.balance += 20 + amount;
+        } else {
+            this.balance += amount;
+        }
+    }
+
+    public String buyLunch(String menuItem){
+        lunchesBought++;
+        if(menuItem.toLowerCase().equals("pizza")){ this.total += 2.50; }
+        if(menuItem.toLowerCase().equals("sandwich")){ this.total += 3.00; }
+        if(menuItem.toLowerCase().equals("fruit")){ this.total += 0.50; }
+        if(menuItem.toLowerCase().equals("drink")){ this.total += 1.00; }
+        if(menuItem.toLowerCase().equals("caviar")){ this.total += 999.00; }
+
+
+        this.balance -= this.total;
+  
+        return (balance > 0) ? "Purchase successful! Your new balance is $" + df.format(balance) : "You do not have enough money." ;
+    }
+
+    public double getTotal(){ return this.total; }
+    public double getBalance(){ return this.balance; }
+
+    public String toString(){
+        return "\nStudent ID: " + idNum + "\nBalance: $" + df.format(balance);
+     }
+}
---
+++ Challenges/LunchAccountTester.java
@@ -0,0 +1,20 @@
+public class LunchAccountTester {
+    public static void main(String[] args){
+        System.out.println("Menu: ");
+        System.out.println("Pizza - $2.50\tSandwich - $3.00\tFruit - $0.50\tDrink - $1.00\tCaviar - $999.00");
+
+        LunchAccount bruh = new LunchAccount();
+        System.out.println(bruh.toString());
+        bruh.addMoney(2.00);
+        System.out.println(bruh.getBalance());
+        System.out.println(bruh.buyLunch("Caviar"));
+
+        LunchAccount huh = new LunchAccount(2.00);
+        System.out.println(huh.toString());
+        huh.addMoney(1.00);
+        System.out.println(huh.toString());
+        System.out.println(huh.buyLunch("Pizza"));
+        System.out.println(huh.toString());
+
+    }
+}
