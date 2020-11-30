55413d5b8dabe73e4c4007c21c2933fe2c512f48
LiAn Song <songlian7@gmail.com>
2020-11-30 11:03:03 -0700

    add example of try/catch handling

--- io/IO.java
+++ io/IO.java
@@ -2,19 +2,35 @@ import java.util.Scanner;
 import java.io.*;
 
 public class IO {
-    public static void main(String[] args) throws IOException, FileNotFoundException  {
+    public static void main(String[] args) {
         System.out.println("Input/Output Example");
 
-        File file = new File("wrong.csv");
-        Scanner scan = new Scanner(file);
+        File file;
+        Scanner scan;
 
         // Exception -> IOException
 
         // 2 ways to handle exceptions (errors)
         // throws -> some other code handles the exception, not my problem
-        // try/catch -> we manage the exception ourselves
+        // put it up next to (string[] args), can separate several with commas
 
-        int num = scan.nextInt();
-        System.out.println("The first number is " + num);
+        // try/catch -> we manage the exception ourselves
+        try {
+            file = new File("wrong.csv");
+            scan = new Scanner(file);
+
+            int num = scan.nextInt();
+            System.out.println("The first number is " + num);
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist");
+            System.exit(0); // quits the program
+        }
+        // } catch (IOException e){
+        //     System.out.println("File could not be read");
+        //     System.exit(0);
+        // } catch (Exception e) {
+        //     System.out.println("General error. Quitting");
+        //     System.exit(0);
+        // }
     }
 }
