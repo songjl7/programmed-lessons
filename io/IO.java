89bc7fe3f06aec838706617b6447add632eaa90b
LiAn Song <songlian7@gmail.com>
2020-11-30 10:55:40 -0700

    exception handling examples/notes

--- io/IO.java
+++ io/IO.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class IO {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException, FileNotFoundException  {
+        System.out.println("Input/Output Example");
+
+        File file = new File("wrong.csv");
+        Scanner scan = new Scanner(file);
+
+        // Exception -> IOException
+
+        // 2 ways to handle exceptions (errors)
+        // throws -> some other code handles the exception, not my problem
+        // try/catch -> we manage the exception ourselves
+
+        int num = scan.nextInt();
+        System.out.println("The first number is " + num);
     }
 }
