b1ad97b6f57cc21a11a9715e17f8b71e61bb1baa
LiAn Song <songlian7@gmail.com>
2020-11-30 11:09:57 -0700

    add data and add while loop

--- io/IO.java
+++ io/IO.java
@@ -8,19 +8,15 @@ public class IO {
         File file;
         Scanner scan;
 
-        // Exception -> IOException
-
-        // 2 ways to handle exceptions (errors)
-        // throws -> some other code handles the exception, not my problem
-        // put it up next to (string[] args), can separate several with commas
-
         // try/catch -> we manage the exception ourselves
         try {
             file = new File("data.csv");
             scan = new Scanner(file);
 
-            int num = scan.nextInt();
-            System.out.println("The first number is " + num);
+            while (scan.hasNextInt()){ // checks if there is more int
+                int num = scan.nextInt();
+                System.out.println("The next number is " + num);
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist");
             System.exit(0); // quits the program
