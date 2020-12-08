269164c4da0b97e45a21d72fab42ac6f9003d977
LiAn Song <songlian7@gmail.com>
2020-12-08 11:24:49 -0700

    add task adding function and displying different menu options

--- Challenges/ToDo.java
+++ Challenges/ToDo.java
@@ -12,39 +12,97 @@ public class ToDo {
         PrintWriter output;
 
         try {
-            output = new PrintWriter("tasks.txt");
+            output = new PrintWriter(new FileWriter("tasks.txt", true));
             file = new File("tasks.txt");
             scan = new Scanner(file);
             userScan = new Scanner(System.in);
 
-            Task myTask = new Task("Clean the kitchen", "12/5/20", false, 5);
-            output.println(myTask);
-
-            System.out.println("Menu: Add new task (a), Update existing task (u), Clear all tasks (c), Quit (x)");
-            char option = userScan.next().charAt(0);
-
-            ArrayList<String> taskInfo = new ArrayList<String>();
-
-            if (option == 'a'){
-                System.out.println("Enter task description");
-                taskInfo.add(userScan.nextLine());
-                System.out.println("Enter due date");
-                taskInfo.add(userScan.nextLine());
-                System.out.println("Enter done status (true/false)");
-                taskInfo.add(userScan.nextLine());
-                System.out.println("Enter priority (1-5)");
-                taskInfo.add(userScan.nextLine());
-            }
-
-            System.out.println(taskInfo);
+            Task newTask;
 
             if (scan.hasNextLine()) {
+                while (scan.hasNextLine()){
+                    System.out.println(scan.nextLine());
+                }
 
+                System.out.println("Menu: Add new task (a), Update existing task (u), Clear all tasks (c), Quit (x)");
+                char option = userScan.nextLine().charAt(0);
+
+                while (option != 'x'){
+                    ArrayList<String> taskInfo = new ArrayList<String>();
+
+                    if (option == 'a') {
+                        System.out.println("Enter task description");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter due date");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter done status (t/f)");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter priority (1-5, with 1 = critical)");
+                        taskInfo.add(userScan.nextLine());
+                    }
+
+                    if (option == 'u'){
+                        
+                    }
+
+                    if (option == 'x'){
+                        scan.close();
+                        userScan.close();
+                        output.close();
+                        System.exit(0);
+                    }
+
+                    newTask = new Task(taskInfo.get(0), taskInfo.get(1), (taskInfo.get(2)).toLowerCase().equals("t"), Integer.parseInt(taskInfo.get(3)));
+
+                    output.println(newTask);
+
+                    System.out.println("Menu: Add new task (a), Update existing task (u), Clear all tasks (c), Quit (x)");
+                    option = userScan.nextLine().charAt(0);
+
+                }
+
+            } else {
+                System.out.println("Menu: Add new task (a), Quit (x)");
+                char option = userScan.nextLine().charAt(0);
+
+                while (option != 'x'){
+                    ArrayList<String> taskInfo = new ArrayList<String>();
+
+                    if (option == 'a') {
+                        System.out.println("Enter task description");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter due date");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter done status (t/f)");
+                        taskInfo.add(userScan.nextLine());
+                        System.out.println("Enter priority (1-5, with 1 = critical)");
+                        taskInfo.add(userScan.nextLine());
+                    }
+
+                    if (option == 'u'){
+                        
+                    }
+
+                    if (option == 'x'){
+                        scan.close();
+                        userScan.close();
+                        output.close();
+                        System.exit(0);
+                    }
+
+                    newTask = new Task(taskInfo.get(0), taskInfo.get(1), (taskInfo.get(2)).toLowerCase().equals("t"), Integer.parseInt(taskInfo.get(3)));
+
+                    output.println(newTask);
+
+                    System.out.println("Menu: Add new task (a), Update existing task (u), Clear all tasks (c), Quit (x)");
+                    option = userScan.nextLine().charAt(0);
+
+                }
             }
 
-            output.close();
             scan.close();
             userScan.close();
+            output.close();
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist");
             System.exit(0);
